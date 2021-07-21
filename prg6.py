1db2c98b375a2c4ec869d810881a660e31d74293
Subhashree8893 <87749962+Subhashree8893@users.noreply.github.com>
2021-07-21 17:46:19 +0530

    add file

---
+++ prg6.py
@@ -0,0 +1,15 @@
+def print_pascaltriangle(size):
+    for i in range(0, size):
+        for j in range(0, i+1):
+            print(number(i, j), end=" ")
+        print()
+def number(n, k):
+    no =1
+    if k> n - k:
+        k = n - k
+    for i in range(0, k):
+        no = no * (n - i)
+        no = no // (i + 1)
+    return no
+rows = 7
+print_pascaltriangle(rows)        
