14cfd1ad3346a77140eda0a3bdb84ab85b114c40
Subhashree8893 <87749962+Subhashree8893@users.noreply.github.com>
2021-07-26 20:06:52 +0530

    add file

---
+++ prg1.py
@@ -0,0 +1,9 @@
+# Referal Id - SIRSS2335
+# Name - Subhashree Sahu
+rows = 5
+num = rows
+for i in range(rows, 0 , -1):
+    for j in range(0, i):
+        print(num, end =' ')
+    print("\n")
+        
